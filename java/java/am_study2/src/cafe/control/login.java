0d6bd00f87a46c8298eaa20d9282ccd9be4c765a
usiwol <dlwlgus535@gmail.com>
2022-11-23 14:11:38 +0900

    fix: 자바와 데이터베이스 연결 수업내용 추가 업로드

--- java/java/am_study2/src/cafe/control/login.java
+++ java/java/am_study2/src/cafe/control/login.java
@@ -6,23 +6,7 @@ public class login implements menu_able {
 
 	@Override
 	public boolean menu_active() {
-		Scanner sc = new Scanner(System.in);		
-		System.out.println("회원가입 양식");
-		
-		//============================
-		
-		System.out.println("아이디 : ");
-		String id = sc.nextLine();
-		System.out.println("이름 : ");
-		String name = sc.nextLine();
-		System.out.println("연락처 : ");
-		String tel = sc.nextLine();
-		System.out.println("이메일 : ");
-		String email = sc.nextLine();
-				
-		//======== 입력 항목 ============
 		
 		return true;
 	}
-
 }
--- java/java/am_study2/src/cafe/control/signin.java
+++ java/java/am_study2/src/cafe/control/signin.java
@@ -4,7 +4,26 @@ public class signin implements menu_able{
 
 	@Override
 	public boolean menu_active() {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);		
+		System.out.println("회원가입 양식");
+		
+		//============================
+		
+		System.out.println("아이디 : ");
+		String id = sc.nextLine();
+		System.out.println("이름 : ");
+		String name = sc.nextLine();
+		System.out.println("연락처 : ");
+		String tel = sc.nextLine();
+		System.out.println("이메일 : ");
+		String email = sc.nextLine();
+				
+		//======== 입력 항목 ============
+		
+		member_DAO mdao = new member_DAO();
+		mdao.member_insert(id, name, tel, email);
+		
 		return true;
 	}
 
