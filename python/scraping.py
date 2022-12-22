f3579eff0e58b80da7ce2018a90d0d7e0678b8d3
usiwol <dlwlgus535@gmail.com>
2022-12-22 12:11:54 +0900

    feat: 크롤링 수업내용 업로드

--- python/scraping.py
+++ python/scraping.py
@@ -71,13 +71,93 @@ import bs4
 #     print(i.text)
 
 
-url = "https://www.naver.com"
+# url = "https://www.naver.com"
+# html = urlopen(url)
+# html = html.read()
+
+# bsp = bs4.BeautifulSoup(html, "html.parser")
+# temp = bsp.findAll("strong",{"class","current"})
+# print(temp[0].text)    #현재기온만 추출하기
+# for t in temp:
+#     if "현재기온" in t:
+#         print(t.text)
+
+# 네이버 메뉴바 추출하기
+# bsp = bs4.BeautifulSoup(html, "html.parser")
+# temp = bsp.findAll("a",{"class","nav"})
+# for menu in temp:
+#     print(menu.text)
+
+
+# url = "https://news.naver.com/main/list.naver?mode=LPOD&mid=sec&sid1=001&sid2=140&oid=001&isYeonhapFlash=Y" #연합뉴스 페이지
+# html = urlopen(url)
+# html = html.read()
+
+# bsp = bs4.BeautifulSoup(html, "html.parser")
+
+# news_ul = bsp.find("ul",{"class","type02"})
+# news_li = news_ul.findAll("li")
+# # print(news_li[0].text)
+
+# for li in news_li:
+#     strong = li.find("strong")
+#     print(strong.text)
+
+
+# 네이버 목요일 웹툰 찾기
+# url = "https://comic.naver.com/webtoon/weekdayList?week=thu"
+# html = urlopen(url)
+# html = html.read()
+
+# bsp = bs4.BeautifulSoup(html, "html.parser")
+
+# webtoon_ul = bsp.find("ul",{"class","img_list"})
+# webtoon_dt = webtoon_ul.findAll("dt")
+# # print(webtoon_dt)
+
+# for a in webtoon_dt:
+#     a = a.find("a")
+#     print(a.text)
+
+# 쌤 답안
+# 요일별로 찾기 쉬운방법
+url = "https://comic.naver.com/webtoon/weekday"
 html = urlopen(url)
 html = html.read()
 
 bsp = bs4.BeautifulSoup(html, "html.parser")
-temp = bsp.findAll("strong",{"class","current"})
-print(temp[0].text)    #현지기온만 추출하기
-for t in temp:
-    if "현재기온" in t:
-        print(t.text)
+
+webtoon_div = bsp.findAll("div",{"class","col_inner"})
+
+ul = webtoon_div[3].find("ul") #목요일 웹툽 
+# print(len(webtoon_div))
+lis = ul.findAll("li")  
+# print(lis[0])
+for li in lis:
+    title = li.find("a",{"class","title"})
+    print(title.text)
+
+
+# webtoon_dt = webtoon_ul.findAll("dt")
+# # print(webtoon_dt)
+
+# for a in webtoon_dt:
+#     a = a.find("a")
+#     print(a.text)
+
+url = "https://www.op.gg/statistics/champions"
+html = urlopen(url)
+html = html.read()
+
+bsp = bs4.BeautifulSoup(html, "html.parser")
+
+table = bsp.findAll("div",{"class","css-hkh81z"})
+# print(len(table))
+tr= table.find("tr") #목요일 웹툽 
+print(len(table))
+# lis = ul.findAll("li")  
+# # print(lis[0])
+# for li in lis:
+#     title = li.find("a",{"class","title"})
+#     print(title.text)
+
